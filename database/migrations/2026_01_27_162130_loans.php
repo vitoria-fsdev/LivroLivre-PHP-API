560bf717a5085ff08cd501ee741a5aaa4cbccd44
rannyzita <fs.vitoria.soares@gmail.com>
2026-01-27 13:30:16 -0300

    feat: Cria a migration de books e de loans.

---
+++ database/migrations/2026_01_27_162130_loans.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loans', function (Blueprint $table){
+            $table->id();
+            $table->unsignedBigInteger('book_id');
+            $table->string('borrower_name');
+            $table->date('loan_date');
+            $table->date('return_date')->nullable();
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::drop('loans');
+    }
+};
